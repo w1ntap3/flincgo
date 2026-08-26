8e7f54855bc9c6554b48bf607ecc5b078b03729b
w1ntap3 <39618610+w1ntap3@users.noreply.github.com>
2026-08-26 23:34:25 +0400

    protocol binary structure

--- esp/main/main.c
+++ esp/main/main.c
@@ -1,3 +1,24 @@
-#include <stdio.h>
+#include <stdint.h>
 
-void app_main(void) { printf("Hello from ESP32 bare metal build!\n"); }
+#define MAGIC_BYTES 4
+
+typedef uint64_t timestamp_t;
+typedef uint16_t item_t;
+
+// note: its all LE bytes
+struct __attribute__((__packed__)) protocol_info {
+  uint8_t magic[MAGIC_BYTES]; // Magic bytes for integrity
+  uint32_t sequence;          // To track lost packets
+  timestamp_t timestamp;      // microseconds since boot
+  uint8_t severity;           // To be parsed on application level e.g.
+  item_t item_id; // Abstract distinction of what causes the log. e.g. sensor,
+                  // thing in IoT..
+  uint16_t payload_len;
+  char payload[]; // A string (encrypted before sending) that contains the main
+                  // message.
+};
+
+void app_main(void) {
+  while (1) {
+  }
+}
