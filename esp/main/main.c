391a353a72d59c426de966f1dbe10c2edf4fe6a0
w1ntap3 <39618610+w1ntap3@users.noreply.github.com>
2026-08-27 16:36:36 +0400

    Add Wi-Fi Kconfig menu and ignore build/secret files

--- esp/main/main.c
+++ esp/main/main.c
@@ -1,10 +1,19 @@
+#include "esp_err.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+#include "sdkconfig.h"
 #include <stdint.h>
 
+// MACRO DEFINITIONS
 #define MAGIC_BYTES 4
 
+// TYPE DEFINITIONS
 typedef uint64_t timestamp_t;
 typedef uint16_t item_t;
 
+// STRUCT DEFINITIONS
 // note: its all LE bytes
 struct __attribute__((__packed__)) protocol_info {
   uint8_t magic[MAGIC_BYTES]; // Magic bytes for integrity
@@ -18,7 +27,19 @@ struct __attribute__((__packed__)) protocol_info {
                   // message.
 };
 
+// STATIC VARIABLE DEFINITIONS
+static wifi_init_config_t wifi_cfg;
+
+// STATIC FUNCTION DECLARATIONS
+static int establish_wifi(const char *ssid, const char *password);
+
 void app_main(void) {
-  while (1) {
-  }
+  ESP_ERROR_CHECK(nvs_flash_init());
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  establish_wifi(CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD);
 }
+
+// STATIC FUNCTION IMPLEMENTATIONS
+static int establish_wifi(const char *ssid, const char *password) { return 0; }
