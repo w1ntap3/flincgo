87a23f31cf90a24622bedd8d853cf6ec4454cf1b
w1ntap3 <39618610+w1ntap3@users.noreply.github.com>
2026-08-28 18:42:21 +0400

    error checking and partial wifi init

--- edge/main/main.c
+++ edge/main/main.c
@@ -1,14 +1,31 @@
 #include "esp_err.h"
 #include "esp_event.h"
+#include "esp_log.h"
 #include "esp_netif.h"
+#include "esp_netif_types.h"
 #include "esp_wifi.h"
+#include "esp_wifi_default.h"
+#include "esp_wifi_types_generic.h"
+#include "freertos/idf_additions.h"
+#include "nvs.h"
 #include "nvs_flash.h"
 #include "sdkconfig.h"
 #include <stdint.h>
+#include <string.h>
 
 // MACRO DEFINITIONS
 #define MAGIC_BYTES 4
+#define NVS_PARTITION "flincgo"
 
+#define AP_SSID CONFIG_WIFI_SSID
+#define AP_PASS CONFIG_WIFI_PASSWORD
+
+#define NVS_TAG "NVS Flash"
+#define EVENT_LOOP_TAG "Default Event Loop"
+#define NETIF_TAG "TCP/IP Stack"
+#define WIFI_TAG "Wi-Fi"
+
+#define WIFI_MAX_RETRIES 10
 // TYPE DEFINITIONS
 typedef uint64_t timestamp_t;
 typedef uint16_t item_t;
@@ -28,18 +45,94 @@ struct __attribute__((__packed__)) protocol_info {
 };
 
 // STATIC VARIABLE DEFINITIONS
-static wifi_init_config_t wifi_cfg;
+static esp_netif_t *ap_netif;
 
-// STATIC FUNCTION DECLARATIONS
-static int establish_wifi(const char *ssid, const char *password);
+static esp_err_t connect_to_collector(const char *ssid, const char *password) {
+  if (AP_SSID == NULL) {
+    ESP_LOGE(WIFI_TAG, "The config has not provided a valid SSID.");
+    return ESP_ERR_WIFI_SSID;
+  }
 
-void app_main(void) {
-  ESP_ERROR_CHECK(nvs_flash_init());
-  ESP_ERROR_CHECK(esp_netif_init());
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  ap_netif = esp_netif_create_default_wifi_ap();
+  wifi_init_config_t wifi_init_cfg = WIFI_INIT_CONFIG_DEFAULT();
+  esp_err_t err = 0;
+  err = esp_wifi_init(&wifi_init_cfg);
+  if (err == ESP_ERR_NO_MEM) {
+    ESP_LOGE(WIFI_TAG, "Could not initialize Wi-Fi.");
+  }
+  ESP_LOGI(WIFI_TAG, "Successfully initialized Wi-Fi.");
 
-  establish_wifi(CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD);
+  // WPA2_PSK wifi Access Point with 1 allowed connection
+  wifi_config_t wifi_cfg = {.ap = {.ssid = AP_SSID,
+                                   .ssid_len = strlen(AP_SSID),
+                                   .ssid_hidden = 0,
+                                   .channel = 0,
+                                   .max_connection = 1,
+                                   .authmode = WIFI_AUTH_WPA2_PSK,
+                                   .password = AP_PASS}};
+  // If password is not set, make the AP open
+  if (strlen(AP_PASS) == 0) {
+    wifi_cfg.ap.authmode = WIFI_AUTH_OPEN;
+  }
+  err = esp_wifi_set_mode(WIFI_MODE_AP);
+  if (err == ESP_ERR_WIFI_NOT_INIT) {
+    ESP_LOGE(WIFI_TAG, "Unexpected error, Wi-Fi was not initialized despite "
+                       "passing the check for initialization");
+  }
+
+  err = esp_wifi_set_config(WIFI_IF_AP, &wifi_cfg);
+  if (err == ESP_ERR_WIFI_STATE) {
+    for (uint8_t retries = 0; retries < WIFI_MAX_RETRIES; retries++) {
+      ESP_LOGW(WIFI_TAG,
+               "Wi-Fi is still connecting while setting a config. Retrying to "
+               "set the config (%d)",
+               retries + 1); // display as retry 1 when the variable is 0, pure
+                             // convenience thing
+      vTaskDelay(1000);
+      err = esp_wifi_set_config(WIFI_IF_AP, &wifi_cfg);
+      if (err != ESP_ERR_WIFI_STATE)
+        break;
+    }
+  }
+  if (err != ESP_OK) {
+    ESP_LOGE(WIFI_TAG,
+             "Config passed to esp_wifi_set_confg() is invalid or "
+             "internal NVS error occured."); // im rushing to my friend, lazy to
+                                             // decouple the error
+    return ESP_ERR_WIFI_NOT_STARTED;
+  }
+  return ESP_OK;
 }
 
-// STATIC FUNCTION IMPLEMENTATIONS
-static int establish_wifi(const char *ssid, const char *password) { return 0; }
+void app_main(void) {
+  esp_err_t err = nvs_flash_init_partition(NVS_PARTITION);
+  err = nvs_flash_init_partition(NVS_PARTITION);
+  if ((err == ESP_ERR_NVS_NO_FREE_PAGES) || (err == ESP_ERR_NO_MEM)) {
+    err = nvs_flash_erase_partition(NVS_PARTITION);
+    if (err != ESP_OK) {
+      ESP_LOGE(NVS_TAG, "Could not initialize NVS Flash.");
+      return;
+    }
+    nvs_flash_init_partition(NVS_PARTITION);
+  }
+  ESP_LOGI(NVS_TAG, "Successfully initialized NVS Flash.");
+
+  err = esp_netif_init();
+  if (err == ESP_FAIL) {
+    ESP_LOGE(NETIF_TAG,
+             "Could not initialize TCP/IP stack (esp-netif module).");
+    return;
+  }
+  ESP_LOGI(NETIF_TAG, "Successfully initialized the TCP/IP stack.");
+  err = esp_event_loop_create_default();
+  if ((err == ESP_ERR_NO_MEM) || (err = ESP_FAIL)) {
+    ESP_LOGE(EVENT_LOOP_TAG, "Could not initialize Default Event Loop.");
+    return;
+  }
+  ESP_LOGI(EVENT_LOOP_TAG, "Successfully initialized Default Event Loop.");
+
+  err = connect_to_collector(AP_SSID, AP_PASS);
+  if (err == ESP_OK) {
+    ESP_LOGI(WIFI_TAG, "Successfully initialized a Wi-Fi Access Point.");
+  }
+}
