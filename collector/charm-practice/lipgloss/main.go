c9cee5f2397c0274106d202cf2ec71843a809eec
kdiffin <vhiunese@gmail.com>
2026-09-07 19:41:28 +0400

    lipgloss lib test

--- collector/charm-practice/lipgloss/main.go
+++ collector/charm-practice/lipgloss/main.go
@@ -1,1 +1,14 @@
 package main
+
+import "charm.land/lipgloss/v2"
+
+var style = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("#FAFAFA")).
+	Background(lipgloss.Color("#7D56F4")).
+	PaddingTop(2).
+	PaddingLeft(4).
+	Width(22)
+
+func main() {
+	lipgloss.Println(style.Render("hello, kitty"))
+}
